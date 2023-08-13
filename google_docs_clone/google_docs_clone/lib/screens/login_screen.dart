ff55e654044af0a04ad3415500d0f516eb206c29
Dejorden <d.moerman@hotmail.nl>
2023-08-13 17:16:23 +0200

    add login screen

---
+++ google_docs_clone/google_docs_clone/lib/screens/login_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton.icon(
+          onPressed: () {},
+          icon: Image.asset('assets/images/g-logo-2.png'),
+          label: const Text(
+            'sign in with Google',
+          ),
+        ),
+      ),
+    );
+  }
+}
